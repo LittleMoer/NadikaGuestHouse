ddc8154d8a8c7a4bdffbdbe8df337f6dd5d9bb28
LittleMoer <mursetyoardiyan@gmail.com>
2025-11-13 22:06:34 +0700

    ganti jam

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -126,7 +126,7 @@ class DashboardController extends Controller
                             $dayEnd = $dayStart->copy()->addDay();
                             $morningStart = $dayStart->copy()->addHours(12);
                             $morningEnd = $dayStart->copy()->addHours(18);
-                            $afternoonStart = $dayStart->copy()->addHours(6);
+                            $afternoonStart = $dayStart->copy()->addHours(18);
                             $afternoonEnd = $dayStart->copy()->addHours(12);
                             $segStart = $row['checkin_at']->greaterThan($dayStart) ? $row['checkin_at'] : $dayStart;
                             $segEnd = $row['checkout_at']->lessThan($dayEnd) ? $row['checkout_at'] : $dayEnd;
