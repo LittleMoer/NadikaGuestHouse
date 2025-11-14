067a25ad298902410c9e8f0daddb474636ce7caa
LittleMoer <mursetyoardiyan@gmail.com>
2025-11-14 14:08:19 +0700

    ukuran id

--- resources/views/booking_printout.blade.php
+++ resources/views/booking_printout.blade.php
@@ -169,7 +169,7 @@
     </div>
 
     <div class="wifi-info">
-      <span style="color: red;">ID:{{ $order->formatted_id }}</span><br>
+      <span style="color: red; font-size: 1.5rem; font-weight: bold;">ID:{{ $order->formatted_id }}</span><br>
       PASSWORD WIFI ATAS: nginapdulu<br>
       Gedung belakang: nadikaguestb2025
     </div>
@@ -297,7 +297,7 @@
     </div>
 
     <div class="wifi-info">
-      <span style="color: red;">ID:{{ $order->formatted_id }}</span><br>
+      <span style="color: red; font-size: 1.5rem; font-weight: bold;">ID:{{ $order->formatted_id }}</span><br>
       PASSWORD WIFI ATAS: nginapdulu<br>
       Gedung belakang: nadikaguestb2025
     </div>
